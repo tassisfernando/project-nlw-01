f25b552dc2a64525e8ea7642d75786f2123b3a70
Tassis <tassis_fernando@hotmail.com>
2020-06-06 15:58:33 -0300

    app

---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import './App.css';
+
+import Routes from './routes';
+
+//JSX: sintaxe de XML dentro do JavaScript
+
+
+function App() {
+ 
+  return (
+    <Routes />
+  );
+}
+
+export default App;
